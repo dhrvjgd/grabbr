d93edc283843da673edbfde9f556143102b1934e
Dhruv Jangid <dhrvjgd@gmail.com>
2026-05-07 18:38:16 +0530

    early release due to security concerns
    feat:
    - single instance of app only
    - remove node from workflow

--- src/main/index.ts
+++ src/main/index.ts
@@ -44,56 +44,72 @@ function createWindow() {
   return mainWindow
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(function () {
-  // Set app user model id for windows
-  electronApp.setAppUserModelId('com.grabbr')
+const gotLock = app.requestSingleInstanceLock()
 
-  if (!is.dev) {
-    Menu.setApplicationMenu(null)
-  }
-
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', function (_, window) {
-    optimizer.watchWindowShortcuts(window)
-  })
-
-  const mainWindow = createWindow()
-
-  // Setup updater and register IPC handlers
-  // ensureSupportedEncoders() // GPU encoding is still under development
-  setupUpdater(mainWindow)
-  registerIpc(mainWindow)
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+if (!gotLock) {
+  app.quit()
+} else {
+  app.on('second-instance', function () {
+    const mainWindow = BrowserWindow.getAllWindows()[0]
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) {
+        mainWindow.restore()
+      }
+      mainWindow.focus()
     }
   })
-})
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.whenReady().then(function () {
+    // Set app user model id for windows
+    electronApp.setAppUserModelId('com.grabbr')
 
-// Before quit, kill all active download processes
-app.on('before-quit', function () {
-  for (const [, proc] of activeDownloadProcesses) {
-    try {
-      proc.kill('SIGTERM')
-    } catch (error) {
-      console.error(error)
+    if (!is.dev) {
+      Menu.setApplicationMenu(null)
     }
-  }
-})
+
+    // Default open or close DevTools by F12 in development
+    // and ignore CommandOrControl + R in production.
+    // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+    app.on('browser-window-created', function (_, window) {
+      optimizer.watchWindowShortcuts(window)
+    })
+
+    const mainWindow = createWindow()
+
+    // Setup updater and register IPC handlers
+    // ensureSupportedEncoders() // GPU encoding is still under development
+    setupUpdater(mainWindow)
+    registerIpc(mainWindow)
+
+    app.on('activate', function () {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    })
+  })
+
+  // Quit when all windows are closed, except on macOS. There, it's common
+  // for applications and their menu bar to stay active until the user quits
+  // explicitly with Cmd + Q.
+  app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+
+  // Before quit, kill all active download processes
+  app.on('before-quit', function () {
+    for (const [, proc] of activeDownloadProcesses) {
+      try {
+        proc.kill('SIGTERM')
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  })
+}
